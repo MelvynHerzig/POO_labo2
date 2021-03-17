72b5568d3ea5f4c641127c8572685c3143ca8722
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-03-17 15:17:45 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/labo2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labo2.dir/main.cpp.obj"
+  "labo2.exe"
+  "labo2.exe.manifest"
+  "labo2.pdb"
+  "liblabo2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labo2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
